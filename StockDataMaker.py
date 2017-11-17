f2dabc16541c0d110a01f170928f8477648e1525
eleeken <like.li@gmail.com>
2017-11-17 23:39:43 +0800

    import project.

---
+++ StockDataMaker.py
@@ -0,0 +1,147 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from six.moves import xrange  # pylint: disable=redefined-builtin
+
+import csv
+import os
+import numpy as np
+import pandas as pd
+
+from tensorflow.python.framework import dtypes
+
+
+class StockDataMaker:
+    """
+    生成证券分析用的数据
+    """
+    COLUMNS = ["Date", "Open", "High", "Low", "Close", "Volume", "Adj Close"]
+    LABEL = "Pred"
+
+    # 每行数据的交易天数
+    TRAININGDATES_PER_PERIOD = 60
+    # 用于计算LABEL的交易天数
+    CHECKDATES_PER_PERIOD = 1
+    # 每个数据的交易总天数
+    DATES_PER_PERIOD = TRAININGDATES_PER_PERIOD + CHECKDATES_PER_PERIOD
+    # 分类总数
+    NUM_CLASSES = 4
+
+    # 训练数据
+    training_set = []
+    # 标签数据
+    label_set = []
+
+    def __init__(self):
+        pass
+
+    def dense_to_one_hot(self, labels_dense, num_classes):
+        """Convert class labels from scalars to one-hot vectors."""
+        num_labels = labels_dense.shape[0]
+        index_offset = np.arange(num_labels) * num_classes
+        labels_one_hot = np.zeros((num_labels, num_classes))
+        labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
+        return labels_one_hot
+
+    def process_label(self, csvset, r):
+
+        close1 = float(csvset.loc[r]['Close'])
+        close2 = float(csvset.loc[r + self.CHECKDATES_PER_PERIOD]['Close'])
+        closegained = (close1 - close2) / close2
+
+        label = ""
+        if closegained > 0.02:
+            label = 0
+        elif closegained > 0.0:
+            label = 1
+        elif closegained > -0.02:
+            label = 2
+        else:
+            label = 3
+        return label
+
+    def csv_to_dataset(self, fn):
+        '''
+        读取CSV历史行情文件，转化为训练测试数据
+        :param fn:文件名
+        :return:
+        '''
+        df = pd.read_csv(fn, skipinitialspace=True, skiprows=1, names=self.COLUMNS)
+        df = df.dropna(axis=0, how='any')
+        df = df[(df['Adj Close'] != 0.0) & (df['Close'] != 0.0) & (df['Volume'] != 0)]
+        df = df.reset_index(drop = True)
+
+        rows_csv = df['Date'].count()
+        if  rows_csv < self.DATES_PER_PERIOD:
+            return
+
+        for j in range(int((rows_csv - self.DATES_PER_PERIOD) / self.DATES_PER_PERIOD)):
+
+            oneperiod_set = []
+            for i in range(j * self.DATES_PER_PERIOD + self.DATES_PER_PERIOD, j * self.DATES_PER_PERIOD + 1, -1):
+
+                vol1 = float(df.loc[i - 1]['Volume'])
+                vol2 = float(df.loc[i]['Volume'])
+                volgained = (vol1 - vol2) / vol2
+
+                open1 = float(df.loc[i - 1]['Open'])
+                open2 = float(df.loc[i]['Open'])
+                opengained = (open1 - open2) / open2
+
+                close1 = float(df.loc[i - 1]['Close'])
+                close2 = float(df.loc[i]['Close'])
+                closegained = (close1 - close2) / close2
+
+                high1 = float(df.loc[i - 1]['High'])
+                high2 = float(df.loc[i]['High'])
+                highgained = (high1 - high2) / high2
+
+                low1 = float(df.loc[i - 1]['Low'])
+                low2 = float(df.loc[i]['Low'])
+                lowgained = (low1 - low2) / low2
+
+                if closegained > 0.101 or closegained < -0.101:
+                    break
+
+                print('%d' % i + ' - ' + '{:f}'.format(volgained) \
+                      + ' - ' + '{:f}'.format(opengained) \
+                      + ' - ' + '{:f}'.format(closegained) \
+                      + ' - ' + '{:f}'.format(highgained) \
+                      + ' - ' + '{:f}'.format(lowgained))
+                oneperiod_set.append(opengained)
+                oneperiod_set.append(closegained)
+                oneperiod_set.append(highgained)
+                oneperiod_set.append(lowgained)
+                oneperiod_set.append(volgained)
+
+            print("size: %d" % len(oneperiod_set))
+
+            if len(oneperiod_set) == self.TRAININGDATES_PER_PERIOD * 5:
+                self.training_set.append(oneperiod_set)
+                label = self.process_label(df, j * self.DATES_PER_PERIOD)
+                self.label_set.append(label)
+
+    def full_read_dataset(self, datadir):
+        """读取数据集
+
+        Args:
+            datadir: 数据文件所在目录
+        Returns:
+            数据集
+        """
+        for fn in os.listdir(datadir):
+            print(">{}".format(fn))
+            self.csv_to_dataset(datadir + "\\" + fn)
+            print("    len:{}".format(len(maker.label_set)))
+
+        self.label_set = np.array(self.label_set, dtype=np.uint8)
+
+        return (self.training_set, self.dense_to_one_hot(self.label_set, self.NUM_CLASSES))
+
+
+if __name__ == '__main__':
+    maker = StockDataMaker()
+    (train, labels) = maker.full_read_dataset("data")
+
+    print(len(maker.label_set))
---
+++ StockDataSpider.py
@@ -0,0 +1,90 @@
+import urllib.request as request
+import urllib.error
+import re
+import time
+import os
+from pandas_datareader import data
+import datetime
+
+class StockDataSpider:
+    def __init__(self):
+        self.STOCKLIST_URL = "http://quote.eastmoney.com/stocklist.html"
+        self.YAHOODATA_URL = 'https://query1.finance.yahoo.com/v7/finance/download/{0}?period1=-252403200&period2=1510848000&interval=1d&events=history&crumb=i4KfR8A.S5F'
+
+    def getListPage(self):
+        '''
+        读取证券列表网页
+        :return: 网页内容
+        '''
+        url = self.STOCKLIST_URL
+        headers = {'User-Agent': 'Mozilla/4.0 (compatible;MSIE 5.5; Windows NT)'}
+        urlreq = request.Request(url, headers=headers)
+        try:
+            respone = request.urlopen(urlreq)
+            content = respone.read().decode("gbk")
+            #print(content)
+
+            return None, content
+        except urllib.error.HTTPError as e:
+            if hasattr(e, "code"):
+                print(u"通迅失败，原因：", e.reason)
+            return e, ""
+        except urllib.error.ContentTooShortError as e:
+            if hasattr(e, "code"):
+                print(u"读取的内容太少，原因：", e.reason)
+            return e, ""
+        except urllib.error.URLError as e:
+            if hasattr(e, "code"):
+                print(u"网址链接不对，原因：", e.reason)
+            return e, ""
+        finally:
+            pass
+
+    def getSaveList(self):
+        '''
+        分析并保存证券列表到文本文件
+        :return:
+        '''
+        e,content = self.getListPage()
+        pattern = re.compile(r'<li>.*?<a.*?target=.*?html">(.*?)\((.*?)\).*?</a></li>', re.S)
+        items = re.findall(pattern, content)
+
+        # print("count:", len(items))
+        f = open('stocklist.txt', 'w')
+        for item in items:
+            if item[1].startswith('600') or item[1].startswith('00'):
+                f.write(item[1] + ',' + item[0] + '\n')
+        f.close()
+
+    def fetchDayDataFromYahoo(self):
+        '''
+
+        :param code: 证券代码
+        :return: 无
+        '''
+        start = datetime.datetime(2010, 1, 1)
+        end = datetime.datetime(2017, 11, 16)
+
+        file = open("stocklist.txt", "r")
+        for line in file:
+            items = line.split(',')
+            print("%s fetching...\n" % items[0])
+            try:
+                if not os.path.isfile('data/' + items[0] + '.csv'):
+                    code = items[0].startswith('0') and items[0] + '.sz' or items[0] + '.ss'
+                    sheet = data.DataReader(code, 'yahoo', start, end)
+                    sheet.to_csv('data/' + items[0] + '.csv')
+                    print('%s file was fetched.\n' % code)
+
+                else:
+                    print('exists.')
+            except Exception as e:
+                print('RemoteDataError!')
+                pass
+
+            time.sleep(1)
+
+if __name__ == '__main__':
+    spider = StockDataSpider()
+    spider.getSaveList()
+    spider.fetchDayDataFromYahoo()
