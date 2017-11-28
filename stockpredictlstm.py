adb236bc754b3545af47b1a19a0dfa0d9e80d67a
eleeken <like.li@gmail.com>
2017-11-28 09:49:00 +0800

    alpha1

--- stockpredictlstm.py
+++ stockpredictlstm.py
@@ -88,31 +88,6 @@ class stockpredictlstm:
         pred = tf.matmul(output, w_out) + b_out
         return pred, final_states
 
-    '''
-    def train_lstm(self, batch_size=80, time_step=15, train_begin=2000, train_end=5800):
-        X = tf.placeholder(tf.float32, shape=[None, time_step, self.INPUTSIZE])
-        Y = tf.placeholder(tf.float32, shape=[None, time_step, self.OUTPUTSIZE])
-        batch_index, train_x, train_y = self.get_train_data(batch_size, time_step, train_begin, train_end)
-        pred, _ = self.LSTM(X)
-
-        loss = tf.reduce_mean(tf.square(tf.reshape(pred, [-1]) - tf.reshape(Y, [-1])))
-        train_op = tf.train.AdamOptimizer(self.LR).minimize(loss)
-        saver = tf.train.Saver(tf.global_variables(), max_to_keep=15)
-        module_file = tf.train.latest_checkpoint('model')
-        with tf.Session() as sess:
-            if module_file != None:
-                saver.restore(sess, module_file)
-
-            for i in range(2000):
-                for step in range(len(batch_index) - 1):
-                    _, loss_ = sess.run([train_op, loss],
-                                        feed_dict={X: train_x[batch_index[step]:batch_index[step + 1]],
-                                                   Y: train_y[batch_index[step]:batch_index[step + 1]]})
-                print(i, loss_)
-                if i % 200 == 0:
-                    print('save model...', saver.save(sess, 'stockpredlstm.model', global_step=i))
-    '''
-
     def train_lstm(self, batch_size=60, time_step=20, train_begin=0, train_end=1800):
         X = tf.placeholder(tf.float32, shape=[None, time_step, self.INPUTSIZE])
         Y = tf.placeholder(tf.float32, shape=[None, time_step, self.OUTPUTSIZE])
@@ -135,31 +110,6 @@ class stockpredictlstm:
             # 我是在window下跑的，这个地址是存放模型的地方，模型参数文件名为modle.ckpt
             # 在Linux下面用 'model_save2/modle.ckpt'
             print("The train has finished")
-
-    '''
-    def prediction(self, time_step=20):
-        X = tf.placeholder(tf.float32, shape=[None, time_step, self.INPUTSIZE])
-        # Y=tf.placeholder(tf.float32, shape=[None,time_step,output_size])
-        mean, std, test_x, test_y = self.get_test_data(time_step)
-        pred, _ = self.LSTM(X)
-        saver = tf.train.Saver(tf.global_variables())
-        with tf.Session() as sess:
-            module_file = tf.train.latest_checkpoint()
-            saver.restore(sess, module_file)
-            test_predict = []
-            for step in range(len(test_x) - 1):
-                prob = sess.run(pred, feed_dict={X: [test_x[step]]})
-                predict = prob.reshape((-1))
-                test_predict.extend(predict)
-            test_y = np.array(test_y) * std[7] + mean[7]
-            test_predict = np.array(test_predict) * std[7] + mean[7]
-            acc = np.average(np.abs(test_predict - test_y[:len(test_predict)]) / test_y[:len(test_predict)])
-
-            plt.figure()
-            plt.plot(list(range(len(test_predict))), test_predict, color='b')
-            plt.plot(list(range(len(test_y))), test_y, color='r')
-            plt.show()
-    '''
 
     def prediction(self, time_step=20, test_begin=1800):
         X = tf.placeholder(tf.float32, shape=[None, time_step, self.INPUTSIZE])
@@ -189,5 +139,5 @@ class stockpredictlstm:
 if __name__ == '__main__':
     predict = stockpredictlstm('train' + os.path.sep + '600004.csv')
     print(predict.datalen)
-    predict.train_lstm(batch_size=60, time_step=20, train_begin=0, train_end=predict.datalen - 500)
-    predict.prediction(time_step=20, test_begin=predict.datalen - 500)
+    predict.train_lstm(batch_size=60, time_step=20, train_begin=0, train_end=predict.datalen - 50)
+    predict.prediction(time_step=20, test_begin=predict.datalen - 50)
